aa842ceecab50cb5c9cd46216274f2dc62fb356a
frscottltd <danny.green@frscott.co.uk>
2026-07-21 13:29:18 +0100

    Add SQL query to retrieve stock information

--- stock_lookup.sql
+++ stock_lookup.sql
@@ -1,1 +1,36 @@
 
+SELECT
+    part AS product_code,
+
+    concat_ws(
+        ' ',
+        nullif(trim(desc1), ''),
+        nullif(trim(desc2), ''),
+        nullif(trim(desc3), ''),
+        nullif(trim(desc4), ''),
+        nullif(trim(desc5), '')
+    ) AS description,
+
+    qty_hand,
+    qty_aloc,
+    qty_pick,
+    qty_order,
+
+    qty_hand - qty_aloc - qty_pick AS estimated_available,
+
+    bin,
+    min_stock,
+    max_stock,
+    reorder,
+    eoq,
+    lead_time,
+    main_supplier,
+    main_mpn,
+    manufacturer
+
+FROM public.stock
+
+WHERE upper(part) = upper($1)
+  AND coalesce(flag_redundant, 0) = 0
+
+ORDER BY company, depot;
